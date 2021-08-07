d8d9b81a4e74d1bcf4b28c8655f5349e44b17d58
Shubham Kumar Jha <56752365+shubhamkumarjha123@users.noreply.github.com>
2021-08-07 23:47:02 +0530

    round off concept

---
+++ folder/Round off.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Round off to nearest
+
+# In[8]:
+
+
+#round off till one decimal point
+
+print(round(5.265, 1))
+print(round(5.276, 1)) 
+print(round(5.273, 1)) 
+
+
+# In[9]:
+
+
+#round off till two decimal point
+
+print(round(5.265, 2))
+print(round(5.276, 2)) 
+print(round(5.273, 2))  
+
+
+# In[7]:
+
+
+#round off till three decimal point
+
+print(round(5.2651, 3))
+print(round(5.2765, 3)) 
+print(round(5.2738, 3))
+
+
+# In[ ]:
+
+
+
+
