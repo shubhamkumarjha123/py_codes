4c35c010ad4240bcee74f11f36edc9ce5a5fa9f6
Shubham Kumar Jha <56752365+shubhamkumarjha123@users.noreply.github.com>
2021-09-19 22:37:23 +0530

    Update disp stars in between string .py

--- folder/disp stars in between string .py
+++ folder/disp stars in between string .py
@@ -3,13 +3,9 @@
 
 # # Display the given string “Hello”,“My”,“Name”, “Is”, “Shubham Kumar Jha” as “Hello**My**Name**Is**Shubham Kumar Jha”
 
-# In[5]:
-
 
 print('Hello','My', 'Name', 'Is', 'Shubham Kumar Jha', sep='**')
 
-
-# In[ ]:
 
 
 
