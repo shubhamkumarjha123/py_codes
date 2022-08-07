aff80b425263f8f17042c952399ddb4fd3a051ec
Shubham Kumar Jha <56752365+shubhamkumarjha123@users.noreply.github.com>
2022-08-07 23:11:26 +0530

    Update Find the sum of the series upto n terms.py

--- folder/Find the sum of the series upto n terms.py
+++ folder/Find the sum of the series upto n terms.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Find the sum of the series upto n terms.
+# # Find the sum of the series upto n terms
 
 # In[1]:
 
 
 n = 5
-# first number of sequence.
+# first number of sequence
 start = 2
 sum_seq = 0
 
@@ -17,7 +17,7 @@ for i in range(0, n):
     sum_seq += start
     # calculate the next term
     start = start * 10 + 2
-print("\nSum of above series is:", sum_seq)
+print("\nSum of above series is:-", sum_seq)
 
 
 # In[ ]:
